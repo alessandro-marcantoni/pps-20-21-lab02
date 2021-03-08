79f7dbf1b7d77289a2e6d49cabed8a3a0426ce4c
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-03-08 13:46:53 +0100

    Part3: Recursion

--- src/lab02/Fibonacci.scala
+++ src/lab02/Fibonacci.scala
@@ -2,4 +2,9 @@ package lab02
 
 object Fibonacci {
 
+  def fib(n: Int): Int = n match {
+    case n if n <= 1 => n
+    case _ => fib(n-1) + fib(n-2)
+  }
+
 }
--- test/lab02/FibonacciTest.scala
+++ test/lab02/FibonacciTest.scala
@@ -1,5 +1,20 @@
 package lab02
 
+import lab02.Fibonacci.fib
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
 class FibonacciTest {
+
+  @Test
+  def fibonacciTest(): Unit = {
+    assertEquals(0, fib(0))
+    assertEquals(1, fib(1))
+    assertEquals(1, fib(2))
+    assertEquals(2, fib(3))
+    assertEquals(3, fib(4))
+    assertEquals(5, fib(5))
+    assertEquals(8, fib(6))
+  }
 
 }
