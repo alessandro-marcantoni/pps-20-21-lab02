3e2466ae98fcc9ea23347b1073312fb527eae801
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-03-08 13:45:36 +0100

    Part2: Currying

--- src/lab02/Currying.scala
+++ src/lab02/Currying.scala
@@ -2,4 +2,26 @@ package lab02
 
 object Currying {
 
+  // CurriedFunType
+  val p1: Double => (Double => (Double => Boolean)) = {
+    x => (y => (z => x <= y && y <= z))
+  }
+
+  // NonCurriedFunType
+  val p2: (Double, Double, Double) => Boolean = (x, y, z) => {
+    x <= y && y <= z
+  }
+
+  // CurriedMethod
+  def p3(x: Double)(y: Double)(z: Double): Boolean = x match {
+    case x if x <= y => (y <= z)
+    case _ => false
+  }
+
+  // NonCurriedMethod
+  def p4(x:Double, y:Double, z:Double): Boolean = x match {
+    case x if x <= y && y <= z => true
+    case _ => false
+  }
+
 }
--- test/lab02/CurryingTest.scala
+++ test/lab02/CurryingTest.scala
@@ -1,5 +1,21 @@
 package lab02
 
+import lab02.Currying._
+import org.junit.jupiter.api.Assertions.{assertFalse, assertTrue}
+import org.junit.jupiter.api.Test
+
 class CurryingTest {
+
+  @Test
+  def testPredicate(): Unit = {
+    assertTrue(p4(10, 20, 30))
+    assertFalse(p4(20, 10, 30))
+  }
+
+  @Test
+  def testPredicateCurrying(): Unit = {
+    assertTrue(p3(10)(20)(30))
+    assertFalse(p3(20)(10)(30))
+  }
 
 }
