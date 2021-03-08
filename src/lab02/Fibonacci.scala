a612df4e33192a70c8b13eba8191fe80dedd1dd7
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-03-08 14:02:31 +0100

    Part3: Tail recursion

--- src/lab02/Fibonacci.scala
+++ src/lab02/Fibonacci.scala
@@ -2,9 +2,14 @@ package lab02
 
 object Fibonacci {
 
-  def fib(n: Int): Int = n match {
-    case n if n <= 1 => n
-    case _ => fib(n-1) + fib(n-2)
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def _fib(n: Int, a: Int, b: Int): Int = n match {
+      case 0 => a
+      case 1 => b
+      case _ => _fib(n - 1, b, a+b)
+    }
+    _fib(n, 0, 1)
   }
 
 }
