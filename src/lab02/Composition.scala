c3cbadbfc63f1ef9bb9bf00396541138fef163d6
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-03-08 13:46:28 +0100

    Part2: Compositions

--- src/lab02/Composition.scala
+++ src/lab02/Composition.scala
@@ -2,4 +2,8 @@ package lab02
 
 object Composition {
 
+  def compose(f: Int => Int, g: Int => Int): Int => Int = {
+    i => f(g(i))
+  }
+
 }
--- test/lab02/CompositionTest.scala
+++ test/lab02/CompositionTest.scala
@@ -1,5 +1,15 @@
 package lab02
 
-class Composition {
+import lab02.Composition._
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CompositionTest {
+
+  @Test
+  def testComposition(): Unit = {
+    assertEquals(20, compose(_*2, _+2)(8))
+    assertEquals(18, compose(_+2, _*2)(8))
+  }
 
 }
