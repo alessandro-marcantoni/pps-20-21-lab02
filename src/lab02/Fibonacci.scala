dcde825274483e4fc419a916d4f1807077ba830f
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-03-08 16:57:51 +0100

    Part3: Refactoring of Fibonacci

--- src/lab02/Fibonacci.scala
+++ src/lab02/Fibonacci.scala
@@ -4,10 +4,10 @@ object Fibonacci {
 
   def fib(n: Int): Int = {
     @annotation.tailrec
-    def _fib(n: Int, a: Int, b: Int): Int = n match {
-      case 0 => a
-      case 1 => b
-      case _ => _fib(n - 1, b, a+b)
+    def _fib(n: Int, first: Int, second: Int): Int = n match {
+      case 0 => first
+      case 1 => second
+      case _ => _fib(n - 1, second, first + second)
     }
     _fib(n, 0, 1)
   }
