dd4998d2b558af9acc96f2e99bedfa1cac39a591
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2021-03-08 17:06:25 +0100

    Part5: Filter

--- test/lab02/ExtendedOptionTest.scala
+++ test/lab02/ExtendedOptionTest.scala
@@ -1,5 +1,18 @@
 package lab02
 
+import lab02.ExtendedOption.Option._
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 class ExtendedOptionTest {
+
+  @Test
+  def filterTest(): Unit = {
+    val predicate: Int => Boolean = _ => true
+
+    assertEquals(Some(5), filter(Some(5))(_ > 2))
+    assertEquals(None(), filter(Some(5))(_ > 8))
+    assertEquals(None(), filter(None())(predicate))
+  }
 
 }
